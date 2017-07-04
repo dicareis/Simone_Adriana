8b5acc4cfd6fd8bb492b6efeeb53af3bb8750a3a
AdrianaReis <dicareis@gmail.com>
2017-07-04 16:59:33 -0400

    fin de la video 3. une methode random ajoutée

--- Simone/GameController.swift
+++ Simone/GameController.swift
@@ -6,10 +6,11 @@
 //  Copyright © 2017 GrassetSucre. All rights reserved.
 //
 
+//=============================================
 import UIKit
-
+//=============================================
 class GameController: UIViewController {
-    
+    //---------------------------------
     @IBOutlet weak var but1: UIButton!
     @IBOutlet weak var but2: UIButton!
     @IBOutlet weak var but3: UIButton!
@@ -20,23 +21,44 @@ class GameController: UIViewController {
     @IBOutlet weak var but8: UIButton!
     @IBOutlet weak var but9: UIButton!
     @IBOutlet weak var scoreKeeper: UILabel!
+    //------------
+    var arrOfButtons: [UIButton]!
+    var arrOfRandomButtons: [UIButton] = []
+    //------------
+    let simoneBrain = SimoneBrain()
     
+    //---------------------------------
     override func viewDidLoad() {
+        //------------
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //------------
+        arrOfButtons = [but1, but2, but3, but4, but5, but6, but7, but8, but9]
+        //------------
+        addRandomButtonToArray()
+        //------------
+    }
+    //---------------------------------
+    
+    func addRandomButtonToArray (){
+        let randomIndex = simoneBrain.getRandomNumber(from: 0, to: arrOfButtons.count-1)
+        arrOfRandomButtons.append(arrOfButtons[randomIndex])
+        
+     
+    
     }
     
+     //---------------------------------
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    //---------------------------------
     
     @IBAction func buttonManager(_ sender: UIButton) {
     
     
     }
-    
+    //---------------------------------
     
     
     
--- Simone/SimoneBrain.swift
+++ Simone/SimoneBrain.swift
@@ -9,6 +9,20 @@
 import Foundation
 //=============================================
 class SimoneBrain{
+    //---------------------------------
+    func getRandomNumber (from f: Int, to t: Int) -> Int {
+        
+        //le UInt32 cast le Int en un entier de 32 bits
+        let from = UInt32(f)
+        let to = UInt32(t)
+        let randomNumber = arc4random_uniform(to - from + 1) + from
+        return Int (randomNumber)
+    }
+    
+    
+    
+    
+    //---------------------------------
     
 }//fin class SimoneBrain
 //=============================================
